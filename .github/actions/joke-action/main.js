c436c52f1397fa57b11399b94251acdcdc146f6f
Dan Hardej <danielhardej@github.com>
2025-01-17 16:43:57 +0800

    Update main.js

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -1,7 +1,13 @@
-const { repoToken, owner, repo, issueNumber } = getInputs();
+const core = require('@actions/core');
+const github = require('@actions/github');
 const request = require("request-promise");
 const getJoke = require("./joke");
 const { Octokit } = require("@octokit/rest");
+
+const repoToken = core.getInput("repoToken");
+const owner = core.getInput("owner");
+const repo = core.getInput("repo");
+const issueNumber = core.getInput("issueNumber");
 
 async function run() {
   const octokit = new Octokit({
