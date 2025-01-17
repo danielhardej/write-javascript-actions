ebd683fa2282c788875589ed54e1e591e3125cb1
Dan Hardej <danielhardej@github.com>
2025-01-17 16:40:51 +0800

    Update to fix things

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -1,14 +1,16 @@
 const { repoToken, owner, repo, issueNumber } = getInputs();
 const request = require("request-promise");
 const getJoke = require("./joke");
+const { Octokit } = require("@octokit/rest");
 
-const octokit = new Octokit({
+async function run() {
+  const octokit = new Octokit({
     auth: repoToken,
-  })
+  });
 
-const joke = await getJoke();
+  const joke = await getJoke();
 
-await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+  await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
     owner: owner,
     repo: repo,
     issue_number: issueNumber,
@@ -16,4 +18,10 @@ await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments
     headers: {
       'X-GitHub-Api-Version': '2022-11-28'
     }
-  })
+  });
+}
+
+run().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
