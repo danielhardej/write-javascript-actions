9b421a9b9d3176a71e8494f0674d3830f095c211
Dan Hardej <danielhardej@github.com>
2025-01-17 17:13:16 +0800

    Update main.js

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -2,23 +2,28 @@ const core = require('@actions/core');
 const getJoke = require("./joke");
 const { Octokit } = require("@octokit/rest");
 
-const repoToken = core.getInput("repoToken");
-const owner = core.getInput("owner");
-const repo = core.getInput("repo");
-const issueNumber = core.getInput("issueNumber");
 
-const joke = getJoke();
+async function run() {
+  const repoToken = core.getInput("repoToken");
+  const owner = core.getInput("owner");
+  const repo = core.getInput("repo");
+  const issueNumber = core.getInput("issueNumber");
 
-const octokit = new Octokit({
-  auth: repoToken
-})
+  const joke = getJoke();
 
-await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
-  owner: owner,
-  repo: repo,
-  issue_number: issueNumber,
-  body: joke,
-  headers: {
-    'X-GitHub-Api-Version': '2022-11-28'
-  }
-})
+  const octokit = new Octokit({
+    auth: repoToken
+  })
+
+  await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+    owner: owner,
+    repo: repo,
+    issue_number: issueNumber,
+    body: joke,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+}
+
+run();
