009834380c4270ebdf4df5b4e56ad46883a0b0de
Dan Hardej <danielhardej@github.com>
2025-01-17 16:49:47 +0800

    Update main.js

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -8,25 +8,19 @@ const owner = core.getInput("owner");
 const repo = core.getInput("repo");
 const issueNumber = core.getInput("issueNumber");
 
-async function run() {
-  const octokit = new Octokit({
-    auth: repoToken,
-  });
 
-  const joke = await getJoke();
+const joke = await getJoke();
 
-  await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
-    owner: owner,
-    repo: repo,
-    issue_number: issueNumber,
-    body: joke,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  });
-}
+const octokit = new Octokit({
+  auth: repoToken,
+})
 
-run().catch(error => {
-  console.error(error);
-  process.exit(1);
-});
+await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+  owner: owner,
+  repo: repo,
+  issue_number: issueNumber,
+  body: joke,
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
